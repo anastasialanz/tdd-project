11f618a818526e9ad0b74b8eb9812bf3b1eb0d60
anastasialanz <anastasia@heysparkbox.com>
2022-02-02 07:36:06 -0700

    first green test

---
+++ js/test_money.js
@@ -0,0 +1,16 @@
+// 5 USD X 2 = 10 USD
+const assert = require('assert');
+
+class Dollar {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  times(multiplier) {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+let fiver = new Dollar(5);
+let tenner = fiver.times(2);
+assert.strictEqual(tenner.amount, 10);
